5e8f1c4b17c985e30cb166d4905c6c19bd6cb5db
Charel Felten <charel.felten@gmail.com>
2024-12-02 17:35:35 +0100

    cleanup

--- kt/src/Main.kt
+++ kt/src/Main.kt
@@ -1,42 +1,7 @@
-import y2024.d01
+import extensions.toInt
+import y2024.d02
 import java.io.File
-import kotlin.math.abs
 
 fun main() {
-    val a0 = File("i24/2").readLines()
-        .map { line -> line.split(" ").map { it.toInt() } }
-        .map { line ->
-            val a = line.windowed(2).map { (v1, v2) ->
-                Pair(v1 - v2 in 1..3, v2 - v1 in 1..3)
-            }.unzip().toList().map { list -> list.all{it} }
-            if (a.max()) 1 else 0
-        }.sum()
-
-    val a1 = File("i24/2").readLines()
-        .map { line -> line.split(" ").map { it.toInt() } }
-        .sumOf { line ->
-            println(line)
-            val lineVs: MutableList<List<Int>> = mutableListOf()
-            repeat(line.size) { i ->
-                val x = line.toMutableList()
-                x.removeAt(i)
-                lineVs.add(x)
-            }
-            lineVs.add(line) // add the original; needs to be safe too
-            val x = lineVs.flatMap { line2 ->
-                print(line2)
-                val a = line2.windowed(2).map { (v1, v2) ->
-                    Pair(v1 - v2 in 1..3, v2 - v1 in 1..3)
-                }.unzip().toList().map { list -> list.all { it } }
-                println(a)
-                a
-            }
-            val res = if (x.max()) 1 else 0
-            println(res)
-            res
-        }
-    println(a1)
-
-
-
+    d02()
 }
---
+++ kt/src/y2024/D02.kt
@@ -0,0 +1,33 @@
+package y2024
+
+import extensions.toInt
+import java.io.File
+
+fun d02() {
+    val input = File("i24/2").readLines()
+        .map { line -> line.split(" ").map { it.toInt() } }
+
+    val a1 = input.sumOf { line ->
+        line.windowed(2).map { (v1, v2) ->
+            Pair(v1 - v2 in 1..3, v2 - v1 in 1..3)
+        }.unzip().toList().any { list -> list.all{ it } }.toInt()
+    }
+
+    val a2 = input.sumOf { line ->
+        val lineVs: MutableList<List<Int>> = mutableListOf()
+        repeat(line.size) { i ->
+            val x = line.toMutableList()
+            x.removeAt(i)
+            lineVs.add(x)
+        }
+        lineVs.add(line) // add the original; needs to be safe too
+        lineVs.flatMap { line2 ->
+            line2.windowed(2).map { (v1, v2) ->
+                Pair(v1 - v2 in 1..3, v2 - v1 in 1..3)
+            }.unzip().toList().map { list -> list.all { it } }
+        }.max().toInt()
+    }
+
+    println(a1)
+    println(a2)
+}
