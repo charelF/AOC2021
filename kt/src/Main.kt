d0885cc33c88aedc40331bbf52f331f61ee3cc8f
Charel Felten <charel.felten@gmail.com>
2024-12-03 10:59:40 +0100

    done

--- kt/src/Main.kt
+++ kt/src/Main.kt
@@ -3,5 +3,16 @@ import y2024.d02
 import java.io.File
 
 fun main() {
-    d02()
+    val mul = """mul\((\d+),(\d+)\)""".toRegex()
+    val disabled = """don\'t\(\).*?(?:do\(\)|${'$'})""".toRegex()
+    val text = File("i24/3")
+        .readText().replace("\n", "-") // important for regex!
+    fun find(input: String): Int {
+        return mul.findAll(input).sumOf { match ->
+            val (x, y) = match.destructured
+            x.toInt() * y.toInt()
+        }
+    }
+    println(find(text))
+    println(find(text.replace(disabled, "")))
 }
