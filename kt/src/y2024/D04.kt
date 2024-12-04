e4dee8d415af0e29146c0c658b295eed79ecfe83
Charel Felten <charel.felten@gmail.com>
2024-12-04 18:35:42 +0100

    cleanup and prep for tomorow

--- kt/src/y2024/D04.kt
+++ kt/src/y2024/D04.kt
@@ -7,68 +7,33 @@ class D04 {
         val a = File("i24/4").readLines().map { line ->
             line.toList()
         }
-
-        var horicount = 0
-        var vertcount = 0
-        var diagcount = 0
-        var mascount = 0
+        var q1c = 0
+        var q2c = 0
         repeat(a.size) { i ->
             repeat(a.size) { j ->
-                println(i to j)
                 try {
                     val hori ="${a[i][j]}${a[i][j+1]}${a[i][j+2]}${a[i][j+3]}"
-                    if (hori == "XMAS" || hori == "SAMX") {
-                        println("hori $hori")
-                        horicount++
-                    }
-                } catch (e: Exception) {
-//                    println(e)
-                }
+                    if (hori == "XMAS" || hori == "SAMX") q1c++
+                } catch(_: Exception) { }
                 try {
                     val vert = "${a[i][j]}${a[i+1][j]}${a[i+2][j]}${a[i+3][j]}"
-                    if (vert == "XMAS" || vert == "SAMX") {
-                        println("vert $vert")
-                        vertcount++
-                    }
-                } catch (e: Exception) {
-//                    println(e)
-                }
+                    if (vert == "XMAS" || vert == "SAMX") q1c++
+                } catch(_: Exception) { }
                 try {
                     val diag = "${a[i][j]}${a[i+1][j+1]}${a[i+2][j+2]}${a[i+3][j+3]}"
-                    if (diag == "XMAS" || diag == "SAMX") {
-                        println("diag $diag")
-                        diagcount++
-                    }
-                } catch (e: Exception) {
-//                    println(e)
-                }
+                    if (diag == "XMAS" || diag == "SAMX") q1c++
+                } catch(_: Exception) { }
                 try {
                     val diag = "${a[i][j]}${a[i+1][j-1]}${a[i+2][j-2]}${a[i+3][j-3]}"
-                    if (diag == "XMAS" || diag == "SAMX") {
-                        println("diag $diag")
-                        diagcount++
-                    }
-                } catch (e: Exception) {
-//                    println(e)
-                }
-
+                    if (diag == "XMAS" || diag == "SAMX") q1c++
+                } catch(_: Exception) { }
                 try {
                     val tor ="${a[i][j]}${a[i+1][j+1]}${a[i+2][j+2]}"
                     val tol ="${a[i][j+2]}${a[i+1][j+1]}${a[i+2][j]}"
-                    if ((tor == "MAS" || tor == "SAM") && (tol == "MAS" || tol == "SAM")) {
-                        println("mascount")
-                        mascount++
-                    }
-                } catch (e: Exception) {
-//                    println(e)
-                }
+                    if ((tor == "MAS" || tor == "SAM") && (tol == "MAS" || tol == "SAM")) q2c++
+                } catch(_: Exception) { }
             }
         }
-
-
-        println(Triple(horicount,vertcount,diagcount))
-        println(horicount + vertcount + diagcount)
-        println(mascount)
-
+        println(q1c to q2c)
     }
 }
---
+++ kt/src/y2024/D05.kt
@@ -0,0 +1,8 @@
+package y2024
+
+class D05 {
+    fun main() {
+        val a = 1
+        println(a)
+    }
+}
