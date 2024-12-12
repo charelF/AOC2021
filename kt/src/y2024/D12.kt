f911ac9881bc30a82918c2a4ff1627a8b29d3bbd
Charel Felten <charel.felten@gmail.com>
2024-12-12 16:18:38 +0100

    p1 done in kotlin

--- kt/src/y2024/D12.kt
+++ kt/src/y2024/D12.kt
@@ -4,33 +4,57 @@ import extensions.*
 import java.io.File
 
 class D12 {
-    fun main() {
-        val result = File("i24/12sss").readLines().map { it.toList() }
-        val plants = result.flatten().distinct()
-        plants.sumOf { plant ->
-            println(plant)
-            val plantCoordinates = result.flatMapIndexed { i, chars ->
-                chars.mapIndexedNotNull { j, ch ->
-                    if (ch == plant) i to j else null
+    val garden = File("i24/12").readLines().map { it.toList() }
+    val m = garden.size
+    val n = garden[0].size
+
+    //    // todo : make this an extension function taking a filter as input
+    fun getAllNeighbours(start: Dual<Int>, constraint: (Dual<Int>) -> Boolean): Set<Dual<Int>> {
+        val cells = mutableSetOf(start)
+        while (true) {
+            val newCells = cells
+                .flatMap { it.getNeighbours(CellDistanceMetric.MANHATTAN) }
+                .filter(constraint)
+                .subtract(cells)
+            cells
+            if (newCells.isEmpty()) break else cells.addAll(newCells)
+        }
+        return cells
+    }
+
+    fun colorGarden(): List<List<Int>> {
+        val coloredGarden = (0 until m).map { (0 until n).map { i -> -1 }.toMutableList() }.toMutableList()
+        var color = 1
+        for (i in 0 until m) {
+            for (j in 0 until n) {
+                if (coloredGarden[i to j] == -1) {
+                    getAllNeighbours(i to j) { it.isWithin(m to n) && garden[it] == garden[i to j] }
+                        .forEach { pair ->
+                            coloredGarden[pair] =
+                                color  // color all neighbours the same and then at the end switch to new color
+                        }.also { color++ }
                 }
             }
-            println("plant coordinates $plantCoordinates")
-            val fenceCoordinates = plantCoordinates
-                .map { it.getNeighbours(CellDistanceMetric.MANHATTAN) }
-//                .filter { !plantCoordinates.contains(it) }
-//                .toSet() subtract plantCoordinates
+        }
+        return coloredGarden
+    }
 
-            println("fence coordinates $fenceCoordinates")
-
-
-
-//            println(fenceCoordinates)
-
-//            println(plantCoordinates.size to fenceCoordinates.size)
-
-            val price = plantCoordinates.size * fenceCoordinates.size
-            println(price)
-            price
+    fun main() {
+        val coloredGarden = colorGarden()
+        coloredGarden.flatten().distinct().sumOf { color ->
+            println("color $color")
+            val coordinates = coloredGarden.mapIndexedNotNull { i, row ->
+                row.mapIndexedNotNull { j, cell ->
+                    if (cell == color) i to j else null
+                }.let { if (it.isEmpty()) null else it }
+            }.flatten().distinct()
+            val neighbours = coordinates.flatMap { coo ->
+                coo.getNeighbours(CellDistanceMetric.MANHATTAN)
+                    .filter { !coordinates.contains(it) }
+                    .also(::println)
+                    .map { nb -> nb to coo }
+            }.distinct()
+            neighbours.size * coordinates.size
         }.also(::println)
     }
 }
