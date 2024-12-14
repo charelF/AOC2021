230b3063a582464ff4ade6a8666e7c3728a32487
Charel Felten <charel.felten@gmail.com>
2024-12-14 13:51:17 +0100

    crazy

--- AOK/src/main/kotlin/y2024/D14.kt
+++ AOK/src/main/kotlin/y2024/D14.kt
@@ -4,7 +4,6 @@ import extensions.*
 import java.io.File
 
 class D14 {
-//    val bounds = 7 to 11
     val bounds = 103 to 101
 
     data class Robot(
@@ -14,9 +13,7 @@ class D14 {
 
     fun Robot.move(steps: Int): Robot {
         var newpos = this.pos
-        repeat(steps) {
-            newpos = (newpos + velocity + bounds) mod bounds
-        }
+        repeat(steps) { newpos = (newpos + velocity + bounds) mod bounds }
         return Robot(newpos, velocity)
     }
 
@@ -27,11 +24,32 @@ class D14 {
             (pos.first < bounds.first/2) && (pos.second > bounds.second/2) -> 3
             (pos.first > bounds.first/2) && (pos.second > bounds.second/2) -> 4
             else -> null
-
         }
     }
 
-
+    fun draw(robots: List<Robot>, i: Int, save: Boolean) {
+        val map = Array(bounds.first) { BooleanArray(bounds.second) { false } }
+        robots.forEach { r -> map[r.pos.first][r.pos.second] = true }
+        val file = File("ressources/y2024/d14/$i.txt")
+        file.parentFile.mkdirs()
+        if (!save) {
+            map.forEach { row ->
+                row.forEach { cell ->
+                    if (cell) print("X") else print(" ")
+                }
+                println()
+            }
+        } else {
+            file.printWriter().use { out ->
+                map.forEach { row ->
+                    row.forEach { cell ->
+                        if (cell) out.print("X") else out.print(" ")
+                    }
+                    out.println()
+                }
+            }
+        }
+    }
 
     val robots = File("../i24/14").readLines().map { line ->
         val (px, py, vx, vy) = Regex("p=(-?\\d+),(-?\\d+) v=(-?\\d+),(-?\\d+)")
@@ -40,16 +58,17 @@ class D14 {
     }
 
     fun main() {
-        robots.mapNotNull { robot ->
-            robot.move(100).quadrant()
-        }.groupBy{ it }.mapValues { it.value.size }.values.reduce(Int::times)
+        robots.mapNotNull { robot -> robot.move(100).quadrant() }
+            .groupBy{ it }
+            .mapValues { it.value.size }
+            .values
+            .reduce(Int::times)
             .also(::println)
+
+        for (i in 7383 until 7384) {
+            draw(robots.map {it.move(i)}, i, save=false)
+        }
     }
-
-//    241798752  too high
-//      241993440
-//    229069152
-
 }
 
 
