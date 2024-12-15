308dff5d43dd964edd0ee82a09cf9ebe178c1106
Charel Felten <charel.felten@gmail.com>
2024-12-15 21:20:09 +0100

    slight cleanup but this will remain the worst code i wrote this year so far (for aoc, did way worse things at work)

--- AOK/src/main/kotlin/y2024/D15.kt
+++ AOK/src/main/kotlin/y2024/D15.kt
@@ -5,70 +5,101 @@ import extensions.*
 
 fun <T> T.print() = println(this)
 
-class D15 {
-    enum class Cell { BOX, WALL, EMPTY }
-    enum class Move { UP, DOWN, RIGHT, LEFT }
+enum class Cell15 { BOX, WALL, EMPTY }
+enum class Move15 { UP, DOWN, RIGHT, LEFT }
 
-    val warehouse: MutableList<MutableList<Cell>>
-    val moves: MutableList<Move>
-    var robot = -1 to -1
+interface D15I {
 
-    private fun nextPos(pos: Dual<Int>, move: Move) = when(move) {
-        Move.UP -> pos.first - 1 to pos.second
-        Move.DOWN -> pos.first + 1 to pos.second
-        Move.RIGHT -> pos.first to pos.second + 1
-        Move.LEFT -> pos.first to pos.second - 1
+    val warehouse: MutableList<MutableList<Cell15>>
+    val moves: MutableList<Move15>
+    var robot: Dual<Int>
+
+    fun nextPos(pos: Dual<Int>, move: Move15) = when(move) {
+        Move15.UP -> pos.first - 1 to pos.second
+        Move15.DOWN -> pos.first + 1 to pos.second
+        Move15.RIGHT -> pos.first to pos.second + 1
+        Move15.LEFT -> pos.first to pos.second - 1
     }
+
+    fun print() {
+        warehouse.forEachIndexed { i, row ->
+            row.forEachIndexed { j, cell ->
+                if (robot == i to j) {
+                    print("@")
+                } else {
+                    when (cell) {
+                        Cell15.EMPTY -> print(".")
+                        Cell15.BOX -> print("O")
+                        Cell15.WALL -> print("#")
+                    }
+                }
+            }
+            println()
+        }
+    }
+
+    fun move()
+    fun gps(): Int
+    fun run(): Int {
+        while(moves.isNotEmpty()) move()
+        return gps()
+    }
+}
+
+class D152: D15I {
+    override val warehouse: MutableList<MutableList<Cell15>>
+    override val moves: MutableList<Move15>
+    override var robot = -1 to -1
 
     init {
         val (p1, p2) = File("../i24/15").readText().split("\n\n")
         warehouse = p1.split("\n").mapIndexed { i, str ->
             str.flatMapIndexed { j, ch ->
                 when (ch) {
-                    '#' -> listOf(Cell.WALL, Cell.WALL)
-                    'O' -> listOf(Cell.BOX, Cell.BOX)
-                    '.' -> listOf(Cell.EMPTY, Cell.EMPTY)
-                    '@' -> { robot = i to j*2; listOf(Cell.EMPTY, Cell.EMPTY) }
+                    '#' -> listOf(Cell15.WALL, Cell15.WALL)
+                    'O' -> listOf(Cell15.BOX, Cell15.BOX)
+                    '.' -> listOf(Cell15.EMPTY, Cell15.EMPTY)
+                    '@' -> { robot = i to j*2; listOf(Cell15.EMPTY, Cell15.EMPTY) }
                     else -> throw Exception("invalid object $ch")
                 }
             }.toMutableList()
         }.toMutableList()
         moves = p2.replace("\n", "").map { ch ->
             when (ch) {
-                '>' -> Move.RIGHT
-                '^' -> Move.UP
-                'v' -> Move.DOWN
-                '<' -> Move.LEFT
+                '>' -> Move15.RIGHT
+                '^' -> Move15.UP
+                'v' -> Move15.DOWN
+                '<' -> Move15.LEFT
                 else -> throw Exception("invalid move >$ch<")
             }
         }.toMutableList()
     }
 
-    fun move() {
+    override fun move() {
         val move = moves.removeFirst()
         val possibleMove = nextPos(robot, move)
         when (warehouse[possibleMove]) {
-            Cell.WALL -> return // do nothing
-            Cell.EMPTY -> robot = possibleMove  // move
-            Cell.BOX -> {
+            Cell15.WALL -> return // do nothing
+            Cell15.EMPTY -> robot = possibleMove  // move
+            Cell15.BOX -> {
                 when(move) { // moving double wide boxes horizontal is easy
-                    Move.RIGHT, Move.LEFT -> {
+                    Move15.RIGHT, Move15.LEFT -> {
                         var after = nextPos(possibleMove, move)
                         while (true) {
                             after = nextPos(after, move)
                             when (warehouse[after]) {
-                                Cell.BOX -> continue
-                                Cell.WALL -> return // nothing we can do
-                                Cell.EMPTY -> {
-                                    warehouse[after] = Cell.BOX  // put a box there (simulate move)
+                                Cell15.BOX -> continue
+                                Cell15.WALL -> return // nothing we can do
+                                Cell15.EMPTY -> {
+                                    warehouse[after] = Cell15.BOX  // put a box there (simulate move)
                                     robot = possibleMove // move
-                                    warehouse[possibleMove] = Cell.EMPTY
+                                    warehouse[possibleMove] = Cell15.EMPTY
                                     return
                                 }
                             }
                         }
                     }
-                    Move.UP, Move.DOWN -> {
+                    Move15.UP, Move15.DOWN -> {
                         // given that we don't separate between left and right box to simplify the above algorithm
                         // we now have to find out if a box is a left or right box
                         val pair = getBoxPair(possibleMove)
@@ -92,21 +123,21 @@ class D15 {
         }
     }
 
-    fun getAffectedBoxes(box: DualDual<Int>, move: Move): List<DualDual<Int>> {
+    private fun getAffectedBoxes(box: DualDual<Int>, move: Move15): List<DualDual<Int>> {
         // get all affected boxes, ignore any other cells
         val row = box.first.first
-        val nextRow = if (move == Move.UP) row - 1 else row + 1
+        val nextRow = if (move == Move15.UP) row - 1 else row + 1
 
         val affRight = nextRow to box.first.second
         val affLeft = nextRow to box.second.second
         val affectedBoxes = mutableSetOf<DualDual<Int>>()
 
-        if (warehouse[affRight] == Cell.BOX) {
+        if (warehouse[affRight] == Cell15.BOX) {
             val affBox = getBoxPair(affRight)
             affectedBoxes.add(affBox)
             affectedBoxes.addAll(getAffectedBoxes(affBox, move))
         }
-        if (warehouse[affLeft] == Cell.BOX) {
+        if (warehouse[affLeft] == Cell15.BOX) {
             val affBox = getBoxPair(affLeft)
             affectedBoxes.add(affBox)
             affectedBoxes.addAll(getAffectedBoxes(affBox, move))
@@ -115,9 +146,9 @@ class D15 {
         return affectedBoxes.toList()
     }
 
-    fun moveAllBoxes(boxes: List<DualDual<Int>>, move: Move): Boolean {
+    private fun moveAllBoxes(boxes: List<DualDual<Int>>, move: Move15): Boolean {
 //        println("about to move $boxes")
-        val newY = if (move == Move.UP) - 1 else + 1
+        val newY = if (move == Move15.UP) - 1 else + 1
         // box = ([yx][yx])
         val newBoxes = boxes.map { box ->
             (box.first.first + newY to box.first.second) to (box.second.first + newY to box.second.second)
@@ -125,173 +156,109 @@ class D15 {
 //        println("towards $newBoxes")
         val cells = newBoxes.flatMap { (left, right) -> listOf(warehouse[left], warehouse[right]) }
 //        println("cells: $cells, ${cells.contains(Cell.WALL)}")
-        if (cells.contains(Cell.WALL)) return false // cant move, so do nothing
+        if (cells.contains(Cell15.WALL)) return false // cant move, so do nothing
         else {
             // can move them
             // delete the current ones
             boxes.forEach { (left, right) ->
-                warehouse[left] = Cell.EMPTY
-                warehouse[right] = Cell.EMPTY
+                warehouse[left] = Cell15.EMPTY
+                warehouse[right] = Cell15.EMPTY
             }
             // redraw the moved ones
             newBoxes.forEach { (left, right) ->
-                warehouse[left] = Cell.BOX
-                warehouse[right] = Cell.BOX
+                warehouse[left] = Cell15.BOX
+                warehouse[right] = Cell15.BOX
             }
         }
         return true
     }
 
-    fun getBoxPair(location: Dual<Int>): DualDual<Int> {
+    private fun getBoxPair(location: Dual<Int>): DualDual<Int> {
         val row = warehouse[location.first]
-        val boxes = row.drop(location.second + 1).takeWhile { it == Cell.BOX }.count()  // no idea why +1
+        val boxes = row.drop(location.second + 1).takeWhile { it == Cell15.BOX }.count()  // no idea why +1
         val isRightBox = boxes % 2 == 0
         return if (isRightBox) (location.first to location.second-1) to location
         else location to (location.first to location.second+1)
     }
 
-    fun gps(): Int {
+    override fun gps(): Int {
         return warehouse.flatMapIndexed { i, rows ->
             rows.mapIndexed { j, cell ->
-                if (cell == Cell.BOX) {
+                if (cell == Cell15.BOX) {
                     val (left, _) = getBoxPair(i to j)
                     100 * left.first + left.second
                 } else 0
             }
         }.sum() / 2
     }
+}
 
-    fun print() {
-        warehouse.forEachIndexed { i, row ->
-            row.forEachIndexed { j, cell ->
-                if (robot == i to j) {
-                    print("@")
-                } else {
-                    when (cell) {
-                        Cell.EMPTY -> print(".")
-                        Cell.BOX -> print("░")
-                        Cell.WALL -> print("#")
+class D151: D15I {
+    override val warehouse: MutableList<MutableList<Cell15>>
+    override val moves: MutableList<Move15>
+    override var robot = -1 to -1
+
+    init {
+        val (p1, p2) = File("../i24/15").readText().split("\n\n")
+        warehouse = p1.split("\n").mapIndexed { i, str ->
+            str.mapIndexed { j, ch ->
+                when (ch) {
+                    '#' -> Cell15.WALL
+                    'O' -> Cell15.BOX
+                    '.' -> Cell15.EMPTY
+                    '@' -> { robot = i to j; Cell15.EMPTY }
+                    else -> throw Exception("invalid object $ch")
+                }
+            }.toMutableList()
+        }.toMutableList()
+        moves = p2.replace("\n", "").map { ch ->
+            when (ch) {
+                '>' -> Move15.RIGHT
+                '^' -> Move15.UP
+                'v' -> Move15.DOWN
+                '<' -> Move15.LEFT
+                else -> throw Exception("invalid move >$ch<")
+            }
+        }.toMutableList()
+    }
+
+    override fun move() {
+        val move = moves.removeFirst()
+        val possibleMove = nextPos(robot, move)
+        when (warehouse[possibleMove]) {
+            Cell15.WALL -> return // do nothing
+            Cell15.EMPTY -> robot = possibleMove  // move
+            Cell15.BOX -> {  // the `move` made us run into the wall, check the next ones
+                var after = possibleMove
+                while (true) {
+                    after = nextPos(after, move)  // check whats behind
+                    when(warehouse[after]) {
+                        Cell15.BOX -> continue
+                        Cell15.WALL -> return // nothing we can do
+                        Cell15.EMPTY -> {
+                            warehouse[after] = Cell15.BOX  // put a box there (simulate move)
+                            robot = possibleMove // move
+                            warehouse[possibleMove] = Cell15.EMPTY
+                            return
+                        }
                     }
                 }
             }
-            println()
         }
     }
 
-    fun main() {
-        print()
-        while(moves.isNotEmpty()) {
-//            println(moves)
-            move()
-//            print()
-        }
-        println(gps())
+    override fun gps(): Int {
+        return warehouse.flatMapIndexed { i, rows ->
+            rows.mapIndexed { j, cell ->
+                if (cell == Cell15.BOX) 100 * i + j else 0
+            }
+        }.sum()
     }
 }
 
-fun main() = D15().main()
 
-//
-//package y2024
-//
-//import java.io.File
-//import extensions.*
-//
-//fun <T> T.print() = println(this)
-//
-//class D15 {
-//    enum class Cell { BOX, WALL, EMPTY }
-//    enum class Move { UP, DOWN, RIGHT, LEFT }
-//
-//    val warehouse: MutableList<MutableList<Cell>>
-//    val moves: MutableList<Move>
-//    var robot = -1 to -1
-//
-//    private fun nextPos(pos: Dual<Int>, move: Move) = when(move) {
-//        Move.UP -> pos.first - 1 to pos.second
-//        Move.DOWN -> pos.first + 1 to pos.second
-//        Move.RIGHT -> pos.first to pos.second + 1
-//        Move.LEFT -> pos.first to pos.second - 1
-//    }
-//
-//    init {
-//        val (p1, p2) = File("../i24/15").readText().split("\n\n")
-//        warehouse = p1.split("\n").mapIndexed { i, str ->
-//            str.mapIndexed { j, ch ->
-//                when (ch) {
-//                    '#' -> Cell.WALL
-//                    'O' -> Cell.BOX
-//                    '.' -> Cell.EMPTY
-//                    '@' -> { robot = i to j; Cell.EMPTY }
-//                    else -> throw Exception("invalid object $ch")
-//                }
-//            }.toMutableList()
-//        }.toMutableList()
-//        moves = p2.replace("\n", "").map { ch ->
-//            when (ch) {
-//                '>' -> Move.RIGHT
-//                '^' -> Move.UP
-//                'v' -> Move.DOWN
-//                '<' -> Move.LEFT
-//                else -> throw Exception("invalid move >$ch<")
-//            }
-//        }.toMutableList()
-//    }
-//
-//    fun move() {
-//        val move = moves.removeFirst()
-//        val possibleMove = nextPos(robot, move)
-//        when (warehouse[possibleMove]) {
-//            Cell.WALL -> return // do nothing
-//            Cell.EMPTY -> robot = possibleMove  // move
-//            Cell.BOX -> {  // the `move` made us run into the wall, check the next ones
-//                var after = possibleMove
-//                while (true) {
-//                    after = nextPos(after, move)  // check whats behind
-//                    when(warehouse[after]) {
-//                        Cell.BOX -> continue
-//                        Cell.WALL -> return // nothing we can do
-//                        Cell.EMPTY -> {
-//                            warehouse[after] = Cell.BOX  // put a box there (simulate move)
-//                            robot = possibleMove // move
-//                            warehouse[possibleMove] = Cell.EMPTY
-//                            return
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//    }
-//
-//    fun gps(): Int {
-//        return warehouse.flatMapIndexed { i, rows ->
-//            rows.mapIndexed { j, cell ->
-//                if (cell == Cell.BOX) 100 * i + j else 0
-//            }
-//        }.sum()
-//    }
-//
-//    fun print() {
-//        warehouse.forEachIndexed { i, row ->
-//            row.forEachIndexed { j, cell ->
-//                if (robot == i to j) {
-//                    print("@")
-//                } else {
-//                    when (cell) {
-//                        Cell.EMPTY -> print(".")
-//                        Cell.BOX -> print("O")
-//                        Cell.WALL -> print("#")
-//                    }
-//                }
-//            }
-//            println()
-//        }
-//    }
-//
-//    fun main() {
-//        while(moves.isNotEmpty()) move()
-//        println(gps())
-//    }
-//}
-//
-//fun main() = D15().main()
+fun main() {
+    val s2 = D152().run()
+    val s1 = D151().run()
+    println(s1 to s2)
+}
