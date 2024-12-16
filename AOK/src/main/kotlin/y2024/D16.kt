084a9ee3defda5ec7d45aceb3dc5624a9bf16b69
Charel Felten <charel.felten@gmail.com>
2024-12-16 11:09:11 +0100

    good morning dijkstra

--- AOK/src/main/kotlin/y2024/D16.kt
+++ AOK/src/main/kotlin/y2024/D16.kt
@@ -21,6 +21,14 @@ class D16 {
         override fun compareTo(other: Edge): Int {
             return this.score.compareTo(other.score)
         }
+        // the two functions below are there so in the visited edges container we know which ones are equal
+        // (same hash) which depends only on loc and dir, not score. this way we dont re-search worse edges
+        override fun equals(other: Any?): Boolean {
+            return (other is Edge) && other.loc == loc && other.dir == dir
+        }
+        override fun hashCode(): Int {
+            return (100000 * loc.hashCode()) + dir.hashCode()
+        }
     }
 
     fun Edge.move(): Dual<Int> {
@@ -44,19 +52,24 @@ class D16 {
     }
 
     fun main() {
-        val puzzle = File("../i24/16s").readLines().map { it.toList() }.print()
+        val puzzle = File("../i24/16").readLines().map { it.toList() }.print()
         val start = puzzle.size - 2 to 1
         val end = 1 to puzzle.first().size -2
         println(puzzle[start] to puzzle[end])
 
         val queue = PriorityQueue<Edge>()
         var edge = Edge(0, start, Direction.RI).print()
+        var nb: List<Edge> = listOf()
         val visited: MutableMap<Edge, Int> = mutableMapOf(edge to 0)
         queue.add(edge)
         while (queue.isNotEmpty()) {
+            if (queue.size % 10000 == 0) {
+                println(queue.size)
+                println(queue.peek())
+            }
             edge = queue.poll()
             if (puzzle[edge.loc] == 'E') break
-            val nb = edge.getNeighbours()
+            nb = edge.getNeighbours()
                 .filter { puzzle[it.loc] != '#' }
                 .filter { e -> e.score < (visited[e] ?: Int.MAX_VALUE) }
             queue.addAll(nb)
