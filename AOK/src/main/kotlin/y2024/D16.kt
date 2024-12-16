30507673051e4b8e7aff39c92fa1992ce03b9fa3
Charel Felten <charel.felten@gmail.com>
2024-12-16 11:31:27 +0100

    beautfiul

--- AOK/src/main/kotlin/y2024/D16.kt
+++ AOK/src/main/kotlin/y2024/D16.kt
@@ -3,11 +3,11 @@ package y2024
 import java.io.File
 import extensions.*
 import java.util.PriorityQueue
+import kotlin.system.measureTimeMillis
 
 class D16 {
     val puzzle = File("../i24/16").readLines().map { it.toList() }
-    val start = puzzle.size - 2 to 1
-    val end = 1 to puzzle.first().size -2
+    val startState = State(puzzle.size - 2 to 1, Direction.RI)
 
     enum class Direction {
         UP { override fun rot() = LE to RI },
@@ -17,25 +17,21 @@ class D16 {
         abstract fun rot(): Dual<Direction>
     }
 
-    data class Edge (
-        val score: Int,
+    data class State (
         val loc: Dual<Int>,
         val dir: Direction
+    )
+
+    data class Edge(
+        val state: State,
+        val score: Int
     ): Comparable<Edge> {
-        override fun compareTo(other: Edge): Int {
-            return this.score.compareTo(other.score)
-        }
-        // the two functions below are there so in the visited edges container we know which ones are equal
-        // (same hash) which depends only on loc and dir, not score. this way we dont re-search worse edges
-        override fun equals(other: Any?): Boolean {
-            return (other is Edge) && other.loc == loc && other.dir == dir
-        }
-        override fun hashCode(): Int {
-            return (100000 * loc.hashCode()) + dir.hashCode()
-        }
+        override fun compareTo(other: Edge) = score.compareTo(other.score)
+        override fun equals(other: Any?) = state.equals(other)
+        override fun hashCode() = state.hashCode()
     }
 
-    fun Edge.move(): Dual<Int> {
+    fun State.move(): Dual<Int> {
         return when(dir) {
             Direction.UP -> loc.first - 1 to loc.second
             Direction.DO -> loc.first + 1 to loc.second
@@ -46,35 +42,38 @@ class D16 {
 
     fun Edge.getNeighbours(): List<Edge> {
         // moving = 1 point; rotating = 1k points; we can rotate or move
-        val dirs = dir.rot()
-        val move = move()
+        val dirs = state.dir.rot()
+        val move = state.move()
         return listOf(
-            Edge(score + 1, move, dir),
-            Edge(score + 1000, loc, dirs.first),
-            Edge(score + 1000, loc, dirs.second)
+            Edge(State(move, state.dir), score + 1),
+            Edge(State(state.loc, dirs.first), score + 1000),
+            Edge(State(state.loc, dirs.second), score + 1000),
         )
     }
 
     fun solve1(): Edge {
-        var edge = Edge(0, start, Direction.RI)
+        var edge = Edge(startState, 0)
         val queue = PriorityQueue<Edge>().also{it.add(edge)}
-        val visited: MutableMap<Edge, Int> = mutableMapOf(edge to 0)
+        val visited: MutableMap<State, Int> = mutableMapOf(edge.state to edge.score)
 
         while (queue.isNotEmpty()) {
             edge = queue.poll()
-            if (puzzle[edge.loc] == 'E') break
+            if (puzzle[edge.state.loc] == 'E') break
             val neighbours = edge.getNeighbours()
-                .filter { puzzle[it.loc] != '#' }
-                .filter { e -> e.score < (visited[e] ?: Int.MAX_VALUE) }
+                .filter { puzzle[it.state.loc] != '#' }
+                .filter { it.score < (visited[it.state] ?: Int.MAX_VALUE) }
             queue.addAll(neighbours)
-            visited.putAll(neighbours.associateWith { it.score })
+            visited.putAll(neighbours.associate {it.state to it.score})
         }
         return edge
     }
 
     fun main() {
-        val s1 = solve1().score
-        println(s1)
+        measureTimeMillis {
+            val s1 = solve1().score
+            println(s1)
+        }.print()
+
     }
 
 //    fun solve2(best: Int) {
