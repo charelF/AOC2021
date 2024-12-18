5cc7c6b1db213cd09aa527e525381d4a5612bfcb
Charel Felten <charel.felten@gmail.com>
2024-12-18 09:54:09 +0100

    ugly but fast dev time

--- AOK/src/main/kotlin/y2024/D18.kt
+++ AOK/src/main/kotlin/y2024/D18.kt
@@ -7,12 +7,11 @@ import y2024.D16.State
 import java.util.PriorityQueue
 
 class D18 {
-    val fallen = 1024//12
     val bytes = File("../i24/18").readLines()
         .flatMap{ it.split(",") }
-        .chunked(2).map { it[0].toInt() to it[1].toInt() }.take(fallen).print()
+        .chunked(2).map { it[0].toInt() to it[1].toInt() }
     val start = 0 to 0
-    val end = 70 to 70 //6 to 6
+    val end = 70 to 70//6 to 6 //70 to 70 //6 to 6
 
     data class Edge(
         val state: Dual<Int>,
@@ -21,7 +20,8 @@ class D18 {
         override fun compareTo(other: D18.Edge): Int = score.compareTo(other.score)
     }
 
-    fun solve1(): Edge {
+    fun solve(firstBytes: List<Dual<Int>>): Edge {
+//        println(firstBytes)
         var edge = Edge(start, 0)
         val queue = PriorityQueue<Edge>().also{ it.add(edge) }
         val visited: MutableMap<Dual<Int>, Int> = mutableMapOf(edge.state to edge.score)
@@ -32,16 +32,35 @@ class D18 {
             val neighbours = edge
                 .state.getNeighbours(DistanceMetric.MANHATTAN)
                 .filter { it.isWithin(end + (1 to 1))}
-                .filter { !bytes.contains(it) }
+                .filter { !firstBytes.contains(it) }
                 .map { Edge(it, edge.score + 1) }
                 .filter { it.score < (visited[it.state] ?: Int.MAX_VALUE) }
             queue.addAll(neighbours)
             visited.putAll(neighbours.associate {it.state to it.score})
         }
+//        println(visited)
         return edge
+    }
+
+    fun main() {
+        solve(bytes.take(1024)).print()
+
+        println("size" to bytes.size)
+        for (i in 1024 until bytes.size) {
+            println(i)
+
+            val first = bytes.take(i)
+//            println("first bytes: ${first.size} last one is ${first.last()}")
+            val edge = solve(first)
+//            println(edge)
+            if (edge.state != end) {
+                println(first.last())
+                break
+            }
+        }
     }
 }
 
 fun main() {
-    D18().solve1().print()
+    D18().main()
 }
