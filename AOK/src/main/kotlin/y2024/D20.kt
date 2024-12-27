2c7ee5a8c8b6007693f083877ab88d63443c1455
Charel Felten <charel.felten@gmail.com>
2024-12-27 11:04:03 +0100

    finally hit the limits of dijkstra; clean up day 20 mess

--- AOK/src/main/kotlin/y2024/D20.kt
+++ AOK/src/main/kotlin/y2024/D20.kt
@@ -3,24 +3,21 @@ package y2024
 import extensions.*
 import java.io.File
 import java.util.PriorityQueue
-import kotlin.math.abs
-
 
 class D20 {
-    val racetrack = File("../i24/20s").readLines().map { it.toList() }
+    val racetrack = File("../i24/20").readLines().map { it.toList()}//.slice(1 until it.lastIndex) }.let { it.slice(1 until it.lastIndex) }
     val bounds = racetrack.size to racetrack.first().size
-    val start = racetrack.dualIndexOf { it == 'S' }.print()
-    val end = racetrack.dualIndexOf { it == 'E' }.print()
-    val startState = State(start, 1)
-    val walls = racetrack.flatMapIndexed { i, row ->
-        row.mapIndexedNotNull { j, ch ->
-            if (ch=='#') i to j else null
-        }
+    val start = racetrack.dualIndexOf { it == 'S' }
+    val end = racetrack.dualIndexOf { it == 'E' }
+
+    sealed class Cheat() {
+        data object AVAILABLE: Cheat()
+        data class EXPIRED(val start: Dual<Int>, val end: Dual<Int>): Cheat()
     }
 
     data class State (
         val pos: Dual<Int>,
-        val cheatsRemaining: Int,
+        val cheat: Cheat
     )
 
     data class Edge(
@@ -30,8 +27,11 @@ class D20 {
         override fun compareTo(other: Edge) = score.compareTo(other.score)
     }
 
-    fun solve(track: List<List<Char>>, limit: Int): Pair<Boolean, Edge> {
-        var edge = Edge(startState, 0)
+    /**
+     * dijkstra finds the shortest path only, we return once we find it
+     */
+    fun dijkstra(): Pair<Boolean, Edge> {
+        var edge = Edge(State(start, Cheat.AVAILABLE), 0)
         val queue = PriorityQueue<Edge>().also{it.add(edge)}
         val visited: MutableMap<State, Int> = mutableMapOf(edge.state to edge.score)
 
@@ -40,9 +40,8 @@ class D20 {
             if (edge.state.pos == end) return true to edge
             val neighbours = edge.state.pos.getNeighbours(DistanceMetric.MANHATTAN)
                 .filter { it.isWithin(bounds) }
-                .map { Edge(State(it, edge.state.cheatsRemaining), edge.score+1) }
-                .filter { track[edge.state.pos] != '#'}
-                .filter { it.score <= limit } // needs to be better than cheat otherwise lame
+                .filter { racetrack[it] != '#'}
+                .map { Edge(State(it, Cheat.AVAILABLE), edge.score+1) }
                 .filter { it.score < (visited[it.state] ?: Int.MAX_VALUE) }
 
             queue.addAll(neighbours)
@@ -51,200 +50,161 @@ class D20 {
         return false to edge
     }
 
-    fun p1() {
-        val shortest = solve(racetrack, Int.MAX_VALUE)
-        println(shortest)
-        println("total walls: ${walls.size}")
-        val improvement = 40
-        walls.mapIndexed { i, wall ->
-            println(i)
-            val track = racetrack.map { it.toMutableList() }.toMutableList()
-            track[wall] = '.'
-            solve(track, shortest.second.score - improvement).first.toInt()
-        }.sum().print()
+
+    /**
+     * with breadth-first-search we can find the distances from each path to the end
+     * by starting at the end and exploring all neighbours recursively
+     */
+    fun bfs(): Map<Dual<Int>, Int> {
+        var pos = end
+        val queue = mutableListOf<Dual<Int>>().also{ it.add(pos) }
+        val discovered: MutableMap<Dual<Int>, Int> = mutableMapOf(pos to 0)
+        while (queue.isNotEmpty()) {
+            pos = queue.removeFirst() // remove first is essential for bfs
+            // we need to use it like a queue
+            // if we removeLast(), its dfs instead
+            val neighbours = pos.getNeighbours(DistanceMetric.MANHATTAN)
+                .filter { it.isWithin(bounds) }
+                .filter { racetrack[it] != '#'}
+                .filter { !discovered.contains(it) }
+
+            queue.addAll(neighbours)
+            discovered.putAll(neighbours.associate {it to discovered[pos]!! + 1})
+        }
+        return discovered
     }
 
     /**
-     * so clean omg
+     * we can also use dijkstra to find all distances if we dont stop once we find the best
+     * path but instead continue until the queue is empty
      */
-    sealed class CheatState() {
-        data object BEFORE: CheatState()
-        data class ACTIVE(val start: Dual<Int>, val possibleEnd: Dual<Int>?): CheatState()
-        data class ENDED(val start: Dual<Int>, val end: Dual<Int>): CheatState()
-    }
-
-    data class State2 (
-        val pos: Dual<Int>,
-        val cheatState: CheatState,
-    )
-
-    data class Edge2 (
-        val state: State2,
-        val score: Int,
-        val cheatDuration: Int // duration isn't part of the state/cheatstate because a cheat is only uniquely identified by its start and end position
-    ): Comparable<Edge2> {
-        override fun compareTo(other: Edge2) = score.compareTo(other.score)
-    }
-
-
-    /**
-     * returns all positions where the cheat could land us
-     */
-    fun cheat(at: Dual<Int>, duration: Int): List<Pair<Dual<Int>, Int>> {
-        val reached = at.getNeighbours(DistanceMetric.MANHATTAN, duration)
-        val distances = reached.map { c -> c to abs(c.first - at.first) + abs(c.second - at.second) }
-        return distances
-    }
-
-    fun solve2(cheatMaxDuration: Int, scoreToBeat: Int): Map<Int,Int> {
-        var edge = Edge2(State2(start, CheatState.BEFORE), 0, 0)
-        val queue = PriorityQueue<Edge2>().also { it.add(edge) }
-        val visited: MutableMap<State2, Int> = mutableMapOf(edge.state to edge.score)
-        val cheats: MutableMap<Int, Int> = mutableMapOf()
+    fun dijkstraDistances(): Map<Dual<Int>, Int> {
+        var edge = Edge(State(end, Cheat.AVAILABLE), 0)
+        val queue = PriorityQueue<Edge>().also{it.add(edge)}
+        val visited: MutableMap<State, Int> = mutableMapOf(edge.state to edge.score)
 
         while (queue.isNotEmpty()) {
             edge = queue.poll()
-            if (edge.state.pos == end) {
-                println(edge)
-                cheats[edge.score] = cheats.computeIfAbsent(edge.score) { 0 } + 1
-
-                val i = (edge.state.cheatState as CheatState.ACTIVE).start
-                val j = (edge.state.cheatState as CheatState.ACTIVE).possibleEnd
-                racetrack.forEachIndexed { ii, line ->
-                    line.forEachIndexed { jj, char ->
-                        if (i == ii to jj) print("1")
-                        else if (j == ii to jj) print("2")
-                        else print(char)
-                    }
-                    println()
-                }
-
-            }
+            if (edge.state.pos == start) continue
             val neighbours = edge.state.pos.getNeighbours(DistanceMetric.MANHATTAN)
                 .filter { it.isWithin(bounds) }
-                .mapNotNull { coords ->
-
-
-
-                    val thisInWall = (racetrack[coords] == '#')
-                    val prevInWall = (racetrack[edge.state.pos] == '#')
-                    val cheatingMustStop = (edge.cheatDuration >= cheatMaxDuration)
-
-
-                    // completely new approach:
-                    // just do regular shortest path
-                    // if not yet cheated, at one moment, just get all neighbours within Manhattan 20
-                    // they must land on . fields. this is the cheeat, then its over
-
-
-
-                    val x = when (edge.state.cheatState) {
-
-                        is CheatState.BEFORE -> {
-                            if (thisInWall) Edge2(
-                                // stupid puzzle only through reddit i find out the cheat has to start outside the wall
-                                State2(coords, CheatState.ACTIVE(edge.state.pos, null)),
-                                edge.score + 1,
-                                edge.cheatDuration + 1
-                            )
-                            else Edge2(State2(coords, CheatState.BEFORE), edge.score + 1, edge.cheatDuration)
-                        }
-
-                        is CheatState.ACTIVE -> {
-                            // from studying the examples it seems that once the cheat starts it doesnt really need to only
-                            // go through walls but can also do normal paths so in other words it just expires the 20
-                            // steps and then its done?
-                            // i think once we start, the cheat goes until it expires
-
-                            // if the cheat is active, we can go through walls or not
-                            // everytime we enter a valid path, we have to record the spot where we left the wall and mark it as possible cheat end
-
-//                            if (thisInWall && prevInWall)
-
-                            if (cheatingMustStop && thisInWall) {
-                                null
-                            } else if (cheatingMustStop && !thisInWall) {
-                                Edge2(
-                                    State2(coords, CheatState.ENDED(edge.state.cheatState.start, edge.state.cheatState.possibleEnd ?: coords)),
-                                    edge.score + 1,
-                                    edge.cheatDuration + 1
-                                )
-                            } else if (thisInWall) {
-                                Edge2(
-                                    State2(coords, edge.state.cheatState),
-                                    edge.score + 1,
-                                    edge.cheatDuration + 1
-                                )
-                            } else if (prevInWall && !thisInWall) {
-                                // the moment we leave
-                                if (coords == 8 to 3) {
-                                    println("-------")
-                                    println(Edge2(
-                                    State2(coords, CheatState.ACTIVE(edge.state.cheatState.start, coords)),
-                                    edge.score + 1,
-                                    edge.cheatDuration + 1
-                                ))
-                                    println(thisInWall)
-                                    println(coords)
-                                    println("\n")
-                                }
-                                Edge2(
-                                    State2(coords, CheatState.ACTIVE(edge.state.cheatState.start, coords)),
-                                    edge.score + 1,
-                                    edge.cheatDuration + 1
-                                )
-                            } else if (!prevInWall && !thisInWall) {
-                                Edge2(
-                                    State2(coords, edge.state.cheatState),
-                                    edge.score + 1,
-                                    edge.cheatDuration + 1
-                                )
-                            } else {
-                                TODO() // shouldnt happen
-                            }
-                        }
-
-                        is CheatState.ENDED -> {
-                            if (thisInWall) null // no no
-                            else Edge2(State2(coords, edge.state.cheatState), edge.score + 1, edge.cheatDuration)
-                        }
-
-
-
-                    }
-
-                    if (coords == (7 to 5) && (edge.state.cheatState is CheatState.ACTIVE && edge.state.cheatState.possibleEnd== 8 to 3)) {
-                        println("%%%%%%%%%%")
-                        println(edge)
-                        println(thisInWall)
-                        println(prevInWall)
-                        println(x)
-                    }
-
-                    x
-                }
-                .filter { it.score < scoreToBeat }
+                .filter { racetrack[it] != '#'}
+                .map { Edge(State(it, Cheat.AVAILABLE), edge.score+1) }
                 .filter { it.score < (visited[it.state] ?: Int.MAX_VALUE) }
 
             queue.addAll(neighbours)
-            visited.putAll(neighbours.associate { it.state to it.score })
+            visited.putAll(neighbours.associate {it.state to it.score})
         }
-        return cheats//.mapKeys { scoreToBeat - it.key }
+        return visited.mapKeys { it.key.pos }
     }
 
-    //Cheats don't need to use all 20 picoseconds; cheats can last any amount of time up to and including 20 picoseconds (but can still only end when the program is on normal track). Any cheat time not used is lost; it can't be saved for another cheat later.
-    // find all the cheats -> a cheat is identified by the start and end position
+    /**
+     * dijkstraCheat uses dijkstra and the fact that neighbours can chose to cheat
+     * to find the best cheated path. However the state space is very big and my implemenation is
+     * inefficient so it only works for the small sample input.
+     */
+    fun dijkstraCheat(cheatTime: Int, timeLimit: Int): Map<Int, Int> {
+        var edge = Edge(State(start, Cheat.AVAILABLE), 0)
+        val queue = PriorityQueue<Edge>().also{ it.add(edge) }
+        val visited: MutableMap<State, Int> = mutableMapOf(edge.state to edge.score)
+        var cheatedResults = mutableMapOf<Int, Int>()
 
-    fun p2() {
-        // fastest without cheating = 84
-//        val shortest = solve2(20, 12).print()
+        while (queue.isNotEmpty()) {
+            edge = queue.poll()
 
-        cheat(3 to 1, 2).print()
+            if (edge.state.pos == end) {
+                cheatedResults[edge.score] = cheatedResults.getOrDefault(edge.score, 0) + 1
+                continue
+            } // but keep searching
+
+            val regularNeighbours = edge.state.pos.getNeighbours(DistanceMetric.MANHATTAN)
+                .filter { it.isWithin(bounds) && racetrack[it] != '#'}
+                .map { Edge(State(it, edge.state.cheat), edge.score+1) }
+                .filter { it.score < (visited[it.state] ?: Int.MAX_VALUE) }
+                .filter { it.score <= timeLimit}
+
+            val cheatedNeighbours = if (edge.state.cheat is Cheat.AVAILABLE) {
+                edge.state.pos.getNeighbours(DistanceMetric.MANHATTAN, 1)
+                    // for cheats, we find all edges which are `cheatTime` away, and where we the first neighbour after
+                    // we start and the last neighbour before we end is a wall. inbetween those we dont care
+                    // and the end must be a non-wall
+                    .filter { it.isWithin(bounds) && racetrack[it] == '#' }
+                    .flatMap { it.getNeighbours(DistanceMetric.MANHATTAN, cheatTime - 2) }
+                    .filter { it.isWithin(bounds) && racetrack[it] == '#' }
+                    .flatMap { it.getNeighbours(DistanceMetric.MANHATTAN, 1) }
+                    .filter { it.isWithin(bounds) && racetrack[it] != '#' }
+                    .map {
+                        Edge(
+                            State(it, Cheat.EXPIRED(edge.state.pos, it)),
+                            edge.score + it.distanceFrom(edge.state.pos, DistanceMetric.MANHATTAN)
+                        )
+                    }
+                    .filter { it.score < (visited[it.state] ?: Int.MAX_VALUE) }
+                    .filter { it.score <= timeLimit }
+            } else listOf()
+
+            val neighbours = (regularNeighbours + cheatedNeighbours).distinct()
+
+            queue.addAll(neighbours)
+            visited.putAll(neighbours.associate { it.state to it.score })
+
+        }
+        return cheatedResults
     }
 
+    /**
+     * Got this idea from reddit - apparently cheats are not really related to walls, except that they must start and
+     * end on a non-wall. The algorithm below just computes the distances between each point ('.') and every other
+     * point within [cheatTime] distance, then checks that the jump between those points (including the distance needed
+     * to walk to them, which is at most [cheatTime]) saves more than [timeToBeSaved] steps. If yes, we count them
+     * as unique cheat shortcut.
+     */
+    fun compareCoordinates(distances: Map<Dual<Int>, Int>, cheatTime: Int, timeToBeSaved: Int): Int {
+        val cheats = mutableMapOf<DualDual<Int>, Int>()
+        racetrack.forEachIndexed { i, line ->
+            line.forEachIndexed { j, ch ->
+                val point = i to j
+                val pointDistance = distances[point]
+                if (pointDistance != null) {
+                    val neighbours = point.getNeighbours(DistanceMetric.MANHATTAN, cheatTime)
+                    neighbours.forEach { nb ->
+                        val neighbourDistance = distances[nb]
+                        if (neighbourDistance != null) {
+                            val saved = (pointDistance - neighbourDistance) - point.distanceFrom(nb, DistanceMetric.MANHATTAN)
+                            if (saved >= timeToBeSaved) {
+                                cheats[(i to j) to nb] = saved
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return cheats.values.groupingBy {it}.eachCount().values.sum()
+    }
+
+    fun main() {
+        val shortest = dijkstra().second.score
+        println("shortest (non cheating path): $shortest")
+        val distances = bfs()
+
+//        measureTimeMillis{
+//            dijkstraCheat(cheatTime = 20, timeLimit = shortest-50).print().also { it.values.sum().print() }
+//        }.print()
+
+        println("part 1: ${compareCoordinates(distances, 2, 100)}")
+        println("part 2: ${compareCoordinates(distances, 20, 100)}")
+
+        // printing
+//        racetrack.mapIndexed { i, line ->
+//            line.mapIndexed { j, ch ->
+//                distances[i to j] ?: '.'
+//            }
+//        }.print2D(pad=5)
+    }
 }
 
-fun main() {
-    D20().p2()
-//    D20().p2()
-}
+fun main() = D20().main()
+
+
+// 1240174 That's not the right answer; your answer is too high
+// 1010263
